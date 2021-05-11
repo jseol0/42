8ac170d9a6828900f76134cf37d28fdee3b59bf2
Jaeyu Seol <jseol@c3r8s5.42seoul.kr>
2021-05-11 16:33:29 +0900

    05111633

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,13 +6,12 @@
 /*   By: jseol <jseol@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/09 09:59:31 by jseol             #+#    #+#             */
-/*   Updated: 2021/05/11 16:05:35 by jseol            ###   ########.fr       */
+/*   Updated: 2021/05/11 16:33:02 by jseol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*
 char		*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ret;
@@ -23,7 +22,7 @@ char		*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	s_len = ft_strlen(s);
 	if (start >= s_len)
-		return(ft_strdup(""));
+		return (ft_strdup(""));
 	ret = (char *)malloc(sizeof(char) * (s_len - start + 1));
 	if (ret == NULL)
 		return (NULL);
@@ -34,30 +33,5 @@ char		*ft_substr(char const *s, unsigned int start, size_t len)
 		i++;
 	}
 	ret[i] = '\0';
-	return (ret);
-}
-*/
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	size_t	i;
-	size_t	substr_length;
-	char	*ret;
-	if (s == NULL)
-		return (NULL);
-	i = 0;
-	while (i < start && s[i] != '\0')
-		i++;
-	substr_length = 0;
-	while (s[i] != '\0' && i - start < len)
-	{
-		substr_length++;
-		i++;
-	}
-	ret = (char *)malloc((substr_length + 1) * sizeof(char));
-	if (ret == NULL)
-		return (NULL);
-	ft_memcpy(ret, s + start, substr_length);
-	ret[substr_length] = '\0';
 	return (ret);
 }
