49c8d41358c0a6d622b52983af4ba4b21b85e10b
Jaeyu Seol <jseol@c3r8s5.42seoul.kr>
2021-05-19 17:02:23 +0900

    parsing

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,19 +6,73 @@
 /*   By: jseol <jseol@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 17:08:25 by jseol             #+#    #+#             */
-/*   Updated: 2021/05/18 17:14:08 by jseol            ###   ########.fr       */
+/*   Updated: 2021/05/19 17:00:30 by jseol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int			ft_printf(const char *format, ...)
-{
-	va_list ap;
-	int		i;
+#include "ft_printf.h"
 
-	va_start(ap, format);
-	int i = 0;
+void	setf(t_format *f)
+{
+	f->zero = 0;
+	f->neg = 0;
+	f->widht = 0;
+	f-> prec = -1;
+}
+
+void	checkoption(char *format, int i, t_format *f, va_list ap)
+{
+	// 정밀도랑 너비 체크하기
+	if (format[i] == '-')
+		f->neg = 1;
+	else if (format[i] == '0')
+		f->zero = 1;
+	else if (format[i] == '.')
+		f->prec = 0;
+	else if (format[i] == '*')
+		f->widht = va_arg(ap, int);
+	else if (format[i] >= '0' && format[i] <= '9')
+		f->widht = (f->widht * 10) + (format[i] - '0');
+
+}
+
+int		get_format(va_list ap, char *format, t_format *f)
+{
+	int	i;
+	int	cnt;
+
+	i = 0;
+	cnt = 0;
+	setf(f);
 	while (format[i])
 	{
-		write(1, format[i], 1);
+		while (format[i] != '%' && format[i] != '\0')
+		{
+			i++;
+			cnt++;
+		}
+		if (format[i] == '%')
+		{
+			while (!ft_strchr(TYPE, format[i]))
+			{
+				checkoption(format, i, f, ap);
+				i++;
+			}
+			f->spec = format[i];
+			cnt += checkprint(format, i, f, ap);
+		}
 	}
+	return (cnt);
+}
+
+int				ft_printf(const char *format, ...)
+{
+	va_list 	ap;
+	int			ret;
+	t_format	f;
+
+	va_start(ap, format);
+	ret = get_format(ap, format, &f);
+	va_end(ap);
+	return (ret);
 }
---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jseol <jseol@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/19 09:59:37 by jseol             #+#    #+#             */
+/*   Updated: 2021/05/19 17:00:34 by jseol            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+# define TYPE "cspdiuxX%"
+
+typedef struct		s_format
+{
+	int				zero;
+	int				neg;
+	int				widht;
+	int				prec;
+	char			spec;
+}					t_format;
+
+int			ft_printf(const char *format, ...);
+
+#endif
