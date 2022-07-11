9a05d9d09b874bb05ecce83318e11ff2a2e49c0a
jseol0@github.com <jseol@c2r1s1.42seoul.kr>
2022-07-11 14:20:28 +0900

    cpp01_ex02

--- cpp_module/01/ex02/main.cpp
+++ cpp_module/01/ex02/main.cpp
@@ -6,13 +6,29 @@
 /*   By: jseol <jseol@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 13:33:33 by jseol             #+#    #+#             */
-/*   Updated: 2022/07/11 13:36:23 by jseol            ###   ########.fr       */
+/*   Updated: 2022/07/11 14:19:31 by jseol            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <iostream>
+#include <iostream>
+#include <string>
 
 int	main(void)
 {
+	std::string		str("HI THIS IS BRAIN");
+	std::string*	stringPTR = &str;
+	std::string& 	stringREF = str;
 
+	std::cout << &str << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+
+	stringREF = "HMM... THIS IS NOT BRAIN";
+	std::cout << str << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+	return (0);
 }
